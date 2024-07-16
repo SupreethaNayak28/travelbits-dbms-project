0282daddf0507a0a68b56301eb8b377f1be5e483
SupreethaNayak28 <107317576+SupreethaNayak28@users.noreply.github.com>
2024-07-16 06:29:44 +0530

    Updated db.php

--- tourism/config/db.php
+++ tourism/config/db.php
@@ -1,3 +1,3 @@
 <?php
-$conn=mysqli_connect('localhost','root','Venk@2802','id22015663_travel') or die('connection failure');
+$conn=mysqli_connect('localhost','root','','tours') or die('connection failure');
 ?>
